6bcae213eb5bc6e6cd75082509f0f5c82ce29c0b
Gaurabjyoti Buragohain <gaurabburagohain2001@gmail.com>
2024-12-18 23:32:00 +0530

    header changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Head from './components/Head';
+import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
-     <h1 className='bg-red-400'>Wanakkam gaurab!!!</h1>
+     {/* <h1 className='bg-red-400'>Wanakkam gaurab!!!</h1> */}
+     <Head />
+     <Body />
     </div>
   );
 }
