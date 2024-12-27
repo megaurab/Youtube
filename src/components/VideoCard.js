20414e5b775405e5cd5d49f6f56d8b31b26e877c
Gaurabjyoti Buragohain <gaurabburagohain2001@gmail.com>
2024-12-27 08:42:26 +0530

    data mining

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -2,16 +2,17 @@ import React from 'react'
 
 const VideoCard = ({info}) => {
   console.log(info);
-  const {statistics, snippet} = info;
-  const {channelTitle, title, thumbnails} = snippet;
+  const {statistics, snippet} = info || {};
+  const {channelTitle, title, thumbnails} = snippet || {};
+  const {likeCount} = statistics;
 
   return (
-    <div>
-      <img src={thumbnails.medium.url}/>
+    <div className='w-44 h-20'>
+      <img className="rounded-lg" src={thumbnails.medium.url}/>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li className='italic'>{ title }</li>
+        <li className='italic'>{ channelTitle }</li>
+        <li className='italic'>{ likeCount }</li>
       </ul>
     </div>
   )
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import { YOUTUBE_VIDEO_API } from '../utils/constants';
-import VideoCard from './VideoCard';
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEO_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 // const keu = process.env.GOOGLE_API_KEY;
 
 const VideoContainer = () => {
-
   const [videos, setVideos] = useState([]);
 
   // const apikey  = process.env.REACT_APP_GOOGLE_API_KEY
   // console.log(apikey);
 
-
-
-  useEffect(()=>{
+  useEffect(() => {
     getVideos();
-  },[]);
+  }, []);
 
- console.log(YOUTUBE_VIDEO_API);
+  console.log(YOUTUBE_VIDEO_API);
 
-  const getVideos = async() =>{
+  const getVideos = async () => {
     const videos = await fetch(YOUTUBE_VIDEO_API);
     const jsonData = await videos.json();
     console.log(jsonData.items);
-    setVideos(jsonData.items)
-  }
+    setVideos(jsonData.items);
+  };
 
   return (
-    <VideoCard info={videos}/>
-  )
-}
+    // <VideoCard info={videos[0]}/>
+    <div className="w-full h-40 flex justify-evenly mt-4">
+      {videos.map((video, index) => {
+        return <VideoCard key={index} info={video} />;
+      })}
+    </div>
+  );
+};
 
-export default VideoContainer
+export default VideoContainer;
